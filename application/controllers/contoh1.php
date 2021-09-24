6ac64ed85d484e3ff67e4aed802edaf2ac89a08e
MasGareng <12200930@bsi.ac.id>
2021-09-24 20:04:25 +0700

    upload pustaka-booking

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+ public function index()
+ {
+ echo "<h1>Perkenalkan</h1>";
+ echo"Nama saya Muhammad Arya Nugraha Saya tingga di daerah Cilebut Jawa Barat olah raga yang saya sukai adalah Sepak Bola";
+ }
+}
---
+++ application/views/v_footer.php
@@ -0,0 +1,7 @@
+<footer>						
+			<label>Ngoding Teros</label>		
+		</footer>
+	</div>
+</body>
+</html>
+
